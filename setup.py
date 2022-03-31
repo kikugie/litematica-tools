d7da27ddcaf913921ac50fd73a2270f35c2a2f82
enjarai <enjarai123@gmail.com>
2022-03-31 20:15:17 +0200

    fixing your shit lmao

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click',
+        'click',
+        'mezmorize',
+        'nbtlib'
     ],
     entry_points={
         'console_scripts': [
