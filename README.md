ae540f41602e74793d96342ebb21c14a5daaefa0
KikuGie <git.kikugie@protonmail.com>
2022-05-08 15:58:07 +0200

    setup update

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # litematica-tools
 Python scripts for doing stuff with litematica files.
 But currently only making advanced material lists.
-
-Usage:
-- CLI not avaiable atm :p
--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='litematica-tools',
-    version='1.0.0',
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'click',
-        'nbtlib'
-    ],
-    entry_points={
-        'console_scripts': [
-            'litematica = litematica_tools.scripts.cli:cli',
-        ],
+    name="litematica-tools",
+    version="1.0.1",
+    author="KikuGie",
+    author_email="kikugie@duck.com",
+    description="Python scripts for interacting with litematica and other formats",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Kikugie/litematica-tools",
+    project_urls={
+        "Bug Tracker": "https://github.com/Kikugie/litematica-tools/issues",
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.10",
+    entry_points={},
+    install_requires=[
+        "nbtlib"
+    ],
+    include_package_data=True
 )
